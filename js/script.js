0d8754f65218fb2e9a396b6533688a570e0e693c
Jan Sebesta <jan.sebesta88@gmail.com>
2023-01-27 14:09:26 +0100

    its only to create object, no idea what is the purpose of this exercise without stage 2

---
+++ js/script.js
@@ -0,0 +1,41 @@
+// The public library, stage 1
+// Let's practice representing data in our web application. We will create a simple library website for which we need an object representing a book.
+
+// Design an object which will represent a book in a public library. The book should have at least these properties
+// title
+// it should allow for multiple authors
+// number of pages
+// publish year
+// whether the book is borrowed or present in the library.
+// Once you have the structure, pick three or four favorite books of yours and create an array of them.
+
+const book = [
+    {
+        title : '12 Rules for Life',
+        author: 'Jordan B. Peterson',
+        numberOfPages: '448',
+        publishYear: '16-01-2016',
+        statusBorrowed: [true,false]
+    }
+    {
+        title : '12 Rules for Life',
+        author: 'Jordan B. Peterson',
+        numberOfPages: '448',
+        publishYear: '16-01-2016',
+        statusBorrowed: [true,false]
+    }
+    {
+        title : '12 Rules for Life',
+        author: 'Jordan B. Peterson',
+        numberOfPages: '448',
+        publishYear: '16-01-2016',
+        statusBorrowed: [true,false]
+    }
+    {
+        title : '12 Rules for Life',
+        author: 'Jordan B. Peterson',
+        numberOfPages: '448',
+        publishYear: '16-01-2016',
+        statusBorrowed: [true,false]
+    }
+]
